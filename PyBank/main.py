868304752b1d00b6f042cd95a84384fc8a18526c
TK <57647644+sundaritk@users.noreply.github.com>
2019-12-21 23:12:01 -0600

    Final Script with output files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,70 @@
-# Main Script
+#Import packages
+import os
+import csv
+import sys
+
+#define input file location
+bankbudget = os.path.join("Resources", "budget_data.csv")
+
+# define output file location
+budgetanalysis = os.path.join("Resources", "budget_analysis.txt")
+
+# initialize variables
+totl_mon = 0
+mon_of_chg = []
+rev_chg_lst = []
+grtest_inc = ["", 0]
+grtest_dec = ["", 9999999999999999999]
+totl_rev = 0
+
+
+#open csv and use it to create lists of header items
+with open(bankbudget) as RevData:
+    reader = csv.DictReader(RevData)
+
+    # extract first row to avoid appending to rev_chg_lst
+    first_row = next(reader)
+    totl_mon= totl_mon+ 1
+    totl_rev = totl_rev + int(first_row["Profit/Losses"])
+    prev_rev = int(first_row["Profit/Losses"])
+    for row in reader:
+   
+        # calculate total
+        totl_mon= totl_mon+ 1
+        totl_rev = totl_rev + int(row["Profit/Losses"])
+
+        # calculate revenue change
+        rev_chg = int(row["Profit/Losses"]) - prev_rev
+        prev_rev = int(row["Profit/Losses"])
+        rev_chg_lst = rev_chg_lst + [rev_chg]
+        mon_of_chg= mon_of_chg+ [row["Date"]]
+
+        # calculate the greatest increase
+        if rev_chg > grtest_inc[1]:
+            grtest_inc[0] = row["Date"]
+            grtest_inc[1] = rev_chg
+
+        # calculate the greatest decrease
+        if rev_chg < grtest_dec[1]:
+            grtest_dec[0] = row["Date"]
+            grtest_dec[1] = rev_chg
+
+# calculate the Average Revenue Change
+rev_avg = sum(rev_chg_lst) / len(rev_chg_lst)
+
+#Print the results and Save Output file
+
+output = (
+        f"\nFinancial Analysis\n"
+        f"----------------------------\n"
+        f"Total Months: {totl_mon}\n"
+        f"Total Revenue: ${totl_rev}\n"
+        f"Average Revenue Change: ${rev_avg}\n"
+        f"Greatest Increase in Revenue: {grtest_inc[0]} (${grtest_inc[1]})\n"
+        f"Greatest Decrease in Revenue: {grtest_dec[0]} (${grtest_dec[1]})")
+
+print(output)
+
+with open(budgetanalysis, "w") as txt_file:
+    txt_file.write(output)
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,75 @@
-# Main script
+# Dependencies
+import csv
+
+#define input file location
+electiondata = "Resources/election_data.csv"
+
+#define output file location
+output = "Resources/election_analysis.txt"
+
+# initialize variables
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+
+# Read input and convert it into a list of dictionaries
+with open(electiondata) as election_data:
+    reader = csv.DictReader(election_data)
+
+    # For each row...
+    for row in reader:
+
+        # Run the loader animation
+        #print(". ", end=""),
+
+        # Add to the total vote count
+        total_votes = total_votes + 1
+        candidate_name = row["Candidate"]
+
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options: 
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0 
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+# Print the results and export the data to text file
+with open(output, "w") as txt_file:
+
+    election_results = (
+    f"\nElection Results\n"
+    f"-------------------------\n"
+    f"Total Votes: {total_votes}\n"
+    f"-------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+
+# Determine the winner by looping through the counts
+    for candidate in candidate_votes:
+
+    # Retrieve vote count and percentage
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+    # Determine winning vote count and candidate
+        if (votes > winning_count):
+             winning_count = votes
+             winning_candidate = candidate
+
+        # Print each candidate's voter count and percentage (to terminal)
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+
+        # Save each candidate's voter count and percentage to text file
+        txt_file.write(voter_output)
+
+    # Print the winning candidate (to terminal)
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
